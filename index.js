2f108a36940b9cdd5b3474e466bf2cdcdfc8af14
araghu9 <54913635+araghu9@users.noreply.github.com>
2024-07-25 17:09:44 -0500

    text

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ function scene1(){
             tooltip.transition().duration(100).style("opacity",0)
         }
         d3.select("svg").append("g").attr("transform","translate("+50+", "+(height+50)+")").append("g").call(d3.axisBottom(scaleX))
+        d3.select("svg").append("text").attr("x", width+70).attr("y", height+60).text("Credit Score")
         d3.select("svg").append("g").attr("transform","translate("+50+", "+50+")").append("g").call(d3.axisLeft(scaleY))
+        d3.select("svg").append("text").attr("x", 10).attr("y", 40).text("Loan Amount")
         d3.select("svg").append("g").attr("transform","translate("+50+", "+50+")").append("g").selectAll("dot").data(data).enter()
        .append("circle").attr("cx", function(d){return scaleX(d["Credit Score"])})
        .attr("cy", function(d){return scaleY(d["Loan Amount"])})
@@ -30,10 +32,10 @@ function scene1(){
                 label: "Despite having 3 previous defaults and large loan, this person is low risk, maybe due to their high credit score",
                 title: "A interesting datapoint in the set"
             },
-            x: 651,
-            y: 47.30000000000001,
-            dy: 47.30000000000001,
-            dx: 651
+            x: 701,
+            y: 97.30000000000001,
+            dy: 47,
+            dx: 90
         }]
         const makeAnnotations = d3.annotation().annotations(annotation)
         d3.select("svg").append("g").attr("class", "annotation-group").call(makeAnnotations)
@@ -47,7 +49,9 @@ function scene2(){
         var scaleX=d3.scaleLinear().domain([20000,120000]).range([0, width])
         var scaleY=d3.scaleLinear().domain([600,800]).range([height,0])
         d3.select("svg").append("g").attr("transform","translate("+50+", "+(height+50)+")").append("g").call(d3.axisBottom(scaleX))
+        d3.select("svg").append("text").attr("x", width+70).attr("y", height+60).text("Income")
         d3.select("svg").append("g").attr("transform","translate("+50+", "+50+")").append("g").call(d3.axisLeft(scaleY))
+        d3.select("svg").append("text").attr("x", 10).attr("y", 40).text("Credit Score")
         d3.select("svg").append("g").attr("transform","translate("+50+", "+50+")").append("g").selectAll("dot").data(data).enter()
         .append("circle").attr("class",function(d){
             if (d["Education Level"].includes(" ")){
@@ -78,13 +82,13 @@ function scene2(){
 
         const annotation=[{
             note: {
-                label: "Despite having 3 previous defaults and large loan, this person is low risk, maybe due to their high credit score",
+                label: "Typically, a higher level of education is considered the path for success, in this case, a lower education has high income, but low credit",
                 title: "A interesting datapoint in the set"
             },
-            x: 651,
-            y: 47.30000000000001,
-            dy: 47.30000000000001,
-            dx: 651
+            x: 737.008,
+            y: 535,
+            dy: -47,
+            dx: 90
         }]
         const makeAnnotations = d3.annotation().annotations(annotation)
         d3.select("svg").append("g").attr("class", "annotation-group").call(makeAnnotations)
@@ -98,7 +102,9 @@ function scene3(){
         var scaleX=d3.scaleLinear().domain([20000,120000]).range([0, width])
         var scaleY=d3.scaleLinear().domain([600,800]).range([height,0])
         d3.select("svg").append("g").attr("transform","translate("+50+", "+(height+50)+")").append("g").call(d3.axisBottom(scaleX))
+        d3.select("svg").append("text").attr("x", width+70).attr("y", height+60).text("Income")
         d3.select("svg").append("g").attr("transform","translate("+50+", "+50+")").append("g").call(d3.axisLeft(scaleY))
+        d3.select("svg").append("text").attr("x", 10).attr("y", 40).text("Credit Score")
         d3.select("svg").append("g").attr("transform","translate("+50+", "+50+")").append("g").selectAll("dot").data(data).enter()
         .append("circle").attr("class",function(d){return d["Marital Status"]}).attr("cx", function(d){return scaleX(Number(d["Income"]))})
        .attr("cy", function(d){return scaleY(Number(d["Credit Score"]))})
@@ -115,13 +121,13 @@ function scene3(){
 
         const annotation=[{
             note: {
-                label: "Despite having 3 previous defaults and large loan, this person is low risk, maybe due to their high credit score",
+                label: "Points such of this seem to have a slightly more closely distributed section here, suggesting more high income but low credit widowers regardless of dependents",
                 title: "A interesting datapoint in the set"
             },
-            x: 651,
-            y: 47.30000000000001,
-            dy: 47.30000000000001,
-            dx: 651
+            x: 748.39,
+            y: 525,
+            dy: -80,
+            dx: 90
         }]
         const makeAnnotations = d3.annotation().annotations(annotation)
         d3.select("svg").append("g").attr("class", "annotation-group").call(makeAnnotations)
@@ -135,12 +141,30 @@ function nextScene(){
     }
     idx+=1
     if (idx%3===0){
+        document.getElementById("info").innerText="This Narrative Visualization will explore a dataset with Financial Data. Specifically, we want to explore if different factors coupled<br>\
+            together leads increased risk profiles. The first chart we will explore is the one below. This one explores the relationship between<br>\
+            credit score and loan amounts. The size of the dots corresponds to the number of defaults the person has had. When we look at this data, we<br>\
+            can't see much. For this reason, we have increased viewing capability for the dots. Hover over any circles and see more details about that<br>\
+            point displayed below."
         scene1()
     }
     else if (idx%3===1){
+        document.getElementById("info").innerText="This chart illustrates the relationship between income and credit score. Using this chart and the previous one,<br>\
+        we can better understand the people at each point, seeing their incomes alongside those credit scores we saw. Furthermore, to add an extra level of detail<br>\
+        to the plot, we can filter on the level education the people in the dataset have had. The points are color coded to the color of the level of education they<br>\
+        have. Beyond that, to isolate these points, if you click on the specific level of education, the points will be toggled on/off. With this capability, we can<br>\
+        see how education has impacted income and the credit score as well. As an example, we can see Bachelor's degree holders tended to have higher income and portentially<br>\
+        better credit at those higher incomes than some other levels of education."
         scene2()
     }
     else if (idx%3===2){
+        document.getElementById("info").innerText="This chart is similar to the one previous one. This is showing the relationship between income and credit score as well,<br>\
+        and allows filtering of color coded points. But there are a few differences to point out here that offer a new dimension to the data and overall understanding of <br>\
+        factors that impact measures of risk like credit scores and risk ratings. One major difference is that the color fitler this time doesn't refer to education level,<br>\
+        but to marital status. This will allow us to filter based on marital status and understand the familial situation behind the person in question and might affect their<br>\
+        score with their income. The other major difference here is how the size of each circle in the plot corresponds to the number of dependants the person has, ranging from<br>\
+        0 to 4. This, similar to marital status, can affect credit score with the income of the person and offers a new dimension to explore, such as how maybe more widowers seem<br>\
+        seem to have lower credit and higher income regardless of dependants which we might not see in other groups."
         scene3()
     }
 }
